0da031729234dac5e4195b8d9a5e1ab8b9c3ccb4
Павел <egevtech@gmail.com>
2025-12-06 21:07:17 +0300

    Добавлен функционал
    ну первая версия получается

--- main.v
+++ main.v
@@ -1,4 +1,25 @@
 module main
 
+import os
+
 fn main() {
+
+	for i in os.args[1..] {
+		list_folder(i, "")
+	}
+
+}
+
+fn list_folder(napath string, indent string) {
+	mut path := os.abs_path(napath)
+	println(indent+"=> ${path} contains:")
+	for i in ( os.ls(path) or {[]} ) {
+		print(indent+"${path}/${i}")
+		if os.is_dir("${path}/${i}") {
+			println(" <= Which is a directory")
+			list_folder("${path}/${i}", indent+"\t")
+		} else {
+			println("")
+		}
+	}
 }
